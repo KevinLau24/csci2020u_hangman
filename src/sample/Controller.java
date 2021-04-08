45791ad13f8aefb5534c536e29f97b7b4df90fba
KevinLau24 <45665650+KevinLau24@users.noreply.github.com>
2021-04-07 23:19:26 -0400

    Logo added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,31 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+
 public class Controller {
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    public void initialize() {
+        File logo = new File("logo.png");
+        Image image = new Image(logo.toURI().toString());
+        imageView.setImage(image);
+    }
+
+    @FXML
+    public void play(){
+
+    }
+
+    @FXML
+    public void exit(){
+        System.exit(0);
+    }
 }
+
