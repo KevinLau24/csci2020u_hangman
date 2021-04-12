1e2754b2f04cdae380401cfe2873667476356aa3
KevinLau24 <45665650+KevinLau24@users.noreply.github.com>
2021-04-11 21:49:56 -0400

    server and client connection

--- src/main/java/org/hangman/ClientThread.java
+++ src/main/java/org/hangman/ClientThread.java
@@ -1,4 +1,25 @@
 package hangman;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketAddress;
+
 public class ClientThread extends Thread {
+    private Socket socket;
+    private SocketAddress socketAddress;
+
+    public ClientThread(Socket socket){
+        this.socket = socket;
+        socketAddress = this.socket.getLocalSocketAddress();
+    }
+
+    public void run(){
+        System.out.println("Connected to server");
+
+        try{
+            socket.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
