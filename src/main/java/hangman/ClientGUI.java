d513ee2192c4456511bc46c811ad31a5511c8d86
ThinhLe188 <thinh.le@ontariotechu.net>
2021-04-12 14:07:41 -0400

    delete unnecessary features

--- src/main/java/hangman/ClientGUI.java
+++ src/main/java/hangman/ClientGUI.java
@@ -22,7 +22,7 @@ import java.net.Socket;
 public class ClientGUI extends Application {
     private double W_HEIGHT = 375, W_WIDTH = 400;
     private double GAME_HEIGHT = 575, GAME_WIDTH = 600;
-    private Scene introScene, menuScene, createGameScene, gameScene;
+    private Scene introScene, menuScene, gameScene;
     private Image logo = new Image("/images/logo.png");
 
     private Socket socket = null;
@@ -30,19 +30,15 @@ public class ClientGUI extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         // Intro scene
-        Label profileNameLb = new Label("Profile Name:");
         Label ipAddressLb = new Label("Server's IP Address:");
         Label portLb = new Label("Server's Port:");
         Label connectStatusLb = new Label();
 
-        TextField profileNameTf = new TextField();
         TextField ipAddressTf = new TextField();
         TextField portTf = new TextField();
 
         Button connectBtn = new Button("Connect");
 
-        VBox profileNameHBox = new VBox(profileNameLb, profileNameTf);
-        profileNameHBox.setSpacing(5);
         VBox ipAddressVBox = new VBox(ipAddressLb, ipAddressTf);
         ipAddressVBox.setSpacing(5);
         VBox portVBox = new VBox(portLb, portTf);
@@ -55,11 +51,10 @@ public class ClientGUI extends Application {
         GridPane introGrid = new GridPane();
         introGrid.setAlignment(Pos.CENTER);
         introGrid.setVgap(10);
-        introGrid.add(profileNameHBox,0,0);
-        introGrid.add(ipAddressVBox,0,1);
-        introGrid.add(portVBox,0,2);
-        introGrid.add(connectBtnHBox,0,4);
-        introGrid.add(connectSttLbHBox,0,3);
+        introGrid.add(ipAddressVBox,0,0);
+        introGrid.add(portVBox,0,1);
+        introGrid.add(connectBtnHBox,0,3);
+        introGrid.add(connectSttLbHBox,0,2);
 
         introScene = new Scene(introGrid, W_WIDTH, W_HEIGHT);
         primaryStage.setScene(introScene);
@@ -82,12 +77,9 @@ public class ClientGUI extends Application {
         HBox logoHBox = new HBox(logoImgV);
         logoHBox.setAlignment(Pos.CENTER);
 
-        Button createGameBtn = new Button("Create Game");
-        createGameBtn.setPrefWidth(100);
-        createGameBtn.setOnAction(e -> primaryStage.setScene(createGameScene));
-        Button findGameBtn = new Button("Find Game");
-        findGameBtn.setPrefWidth(100);
-        Button exitBtn = new Button("Exit Game");
+        Button playBtn = new Button("Play");
+        playBtn.setPrefWidth(100);
+        Button exitBtn = new Button("Exit");
         exitBtn.setPrefWidth(100);
         exitBtn.setOnAction(e -> {
             try {
@@ -99,55 +91,17 @@ public class ClientGUI extends Application {
         });
 
         VBox menuBtnVBox = new VBox();
-        menuBtnVBox.getChildren().addAll(createGameBtn, findGameBtn, exitBtn);
+        menuBtnVBox.getChildren().addAll(playBtn, exitBtn);
         menuBtnVBox.setAlignment(Pos.CENTER);
         menuBtnVBox.setSpacing(10);
 
         GridPane menuGrid = new GridPane();
-        menuGrid.setAlignment(Pos.TOP_CENTER);
+        menuGrid.setAlignment(Pos.CENTER);
         menuGrid.setVgap(10);
         menuGrid.add(logoHBox,0,0);
         menuGrid.add(menuBtnVBox,0,1);
 
         menuScene = new Scene(menuGrid, W_WIDTH, W_HEIGHT);
-
-
-        // 'Create game' scene
-        Label numPlayerLb = new Label("Number of players: ");
-
-        ComboBox numPlayersCb = new ComboBox();
-        numPlayersCb.getItems().addAll(1, 2, 3, 4);
-
-        Button backBtn1 = new Button("Back");
-        backBtn1.setOnAction(event -> {
-            numPlayersCb.valueProperty().set(null);
-            primaryStage.setScene(menuScene);
-        });
-        backBtn1.setPrefWidth(50);
-        Button createBtn = new Button("Create");
-        createBtn.setPrefWidth(50);
-
-        HBox backBtnHBox1 = new HBox(backBtn1);
-        backBtnHBox1.setPrefWidth(W_WIDTH);
-        HBox numPlayersHBox = new HBox(numPlayerLb, numPlayersCb);
-        numPlayersHBox.setSpacing(5);
-        HBox createBtnHBox = new HBox(createBtn);
-        createBtnHBox.setAlignment(Pos.CENTER);
-
-        GridPane innerGrid = new GridPane();
-        innerGrid.setAlignment(Pos.TOP_CENTER);
-        innerGrid.setVgap(10);
-        innerGrid.add(numPlayersHBox,0,0, 2,1);
-        innerGrid.add(createBtnHBox,0,2, 2, 1);
-
-        GridPane createGameGrid = new GridPane();
-        createGameGrid.setAlignment(Pos.TOP_CENTER);
-        createGameGrid.setPadding(new Insets(10, 10, 10, 10));
-        createGameGrid.setVgap(10);
-        createGameGrid.add(backBtnHBox1,0,0);
-        createGameGrid.add(innerGrid,0,1);
-
-        createGameScene = new Scene(createGameGrid, W_WIDTH, W_HEIGHT);
 
 
         // Game scene
