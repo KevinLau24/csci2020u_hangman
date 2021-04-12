cff329d3eee23f9b48fbb00483aaf551fdb1eed4
kennethtse14 <tse.kenneth79@gmail.com>
2021-04-11 21:13:07 -0400

    server code

--- src/main/java/org/hangman/Server.java
+++ src/main/java/org/hangman/Server.java
@@ -1,4 +1,43 @@
 package hangman;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+
+    protected Socket clientSocket = null;
+    protected ServerSocket serverSocket = null;
+    //protected ClientThread[] threads = null;
+    protected int numClients = 0;
+
+    public static int SERVER_PORT = 8080;
+    public static int MAX_CLIENTS = 25;
+
+    public Server(){
+        try{
+            serverSocket = new ServerSocket(SERVER_PORT);
+            //threads = new ClientThread[MAX_CLIENTS];
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void run(){
+        try{
+            while(true){
+                clientSocket = serverSocket.accept();
+                //threads[numClients] = new ClientThread(clientSocket);
+                //threads[numClients].start();
+                //numClients++;
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+        Server server = new Server();
+        server.run();
+    }
 }
